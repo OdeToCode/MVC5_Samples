6bb08880518f4af0a03983e8de38caea000d866b
K. Scott Allen <scott@OdeToCode.com>
2013-10-06 17:01:17 -0700

    simple katana middle ware component

--- katana/KatanaMiddleware/Program.cs
+++ katana/KatanaMiddleware/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,38 @@ namespace KatanaMiddleware
     {
         static void Main(string[] args)
         {
+            string uri = "http://localhost:8080/";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                // pre handler
+                Console.WriteLine("Getting request " + ctx.Request.Path);
+
+                await next();
+
+                // post handler
+
+                Console.WriteLine("\tSending response: " + ctx.Response.StatusCode);
+
+            });
+
+            app.Run(ctx =>
+            {                
+                return ctx.Response.WriteAsync("Hello!");
+            });        
         }
     }
 }
