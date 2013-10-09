5effc8b53db2f86b53f4a9f3a5bd9ce70a01cf38
K. Scott Allen <scott@OdeToCode.com>
2013-10-09 07:33:34 -0700

    basic bootstrap site and option remove styles

---
+++ Bootstrap/Bootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap.Startup))]
+namespace Bootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
