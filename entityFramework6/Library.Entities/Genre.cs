c3830c22567eb94794a0146aedd61ce445df70de
K. Scott Allen <scott@OdeToCode.com>
2013-10-18 05:21:25 -0700

    refactor enum reflection for genre

--- entityFramework6/Library.Entities/Genre.cs
+++ entityFramework6/Library.Entities/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace Library.Entities
 {
      public enum Genre
     {
+        [Display(Name="Non Fiction")]
         NonFiction,
         Romance, 
-        Action, 
+        Action,
+        [Display(Name = "Science Fiction")]
         ScienceFiction
     }
 }
---
+++ entityFramework6/Library.Web/Helpers/EnumHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Web.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static IEnumerable<SelectListItem> EnumToItems(this Type enumType, object selected)
+        {
+            if (!typeof(Enum).IsAssignableFrom(enumType))
+            {
+                throw new ArgumentException("Type must be an enum");
+            }
+
+            var names = Enum.GetNames(enumType);
+            var values = Enum.GetValues(enumType).Cast<object>();
+
+            var items =  names.Zip(values, (name, value) =>
+                new SelectListItem
+                {
+                    Text = GetName(enumType, name),
+                    Value = value.ToString(),
+                    Selected = value == selected
+                }
+            );
+
+            return items;
+        }
+
+        static string GetName(Type enumType, string name)
+        {
+            var result = name;
+
+            var attribute = enumType.GetField(name)
+                                .GetCustomAttributes(inherit: false)
+                                .OfType<DisplayAttribute>()
+                                .FirstOrDefault();
+            if (attribute != null && !String.IsNullOrEmpty(attribute.Name))
+            {
+                result = attribute.Name;
+            }
+
+            return result;
+        }
+    }
+}
