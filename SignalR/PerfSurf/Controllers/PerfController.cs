23f141b06000824ea19a59c386816d9a2ac363d8
K. Scott Allen <scott@OdeToCode.com>
2013-10-20 15:34:35 +0000

    binding performance couners

---
+++ SignalR/PerfSurf/Controllers/PerfController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+
+namespace PerfSurf.Controllers
+{
+    public class PerfController : ApiController
+    {
+        [Route("perf/categories")]
+        public IHttpActionResult GetPerformanceCategories()
+        {
+            var model = PerformanceCounterCategory
+                .GetCategories()
+                .OrderBy(c => c.CategoryName)
+                .Select(c => CleanName(c.CategoryName));
+
+            return Ok(model);
+        }
+     
+        [Route("perf/{category}/counters")]
+        public IHttpActionResult GetCounters(string category)
+        {
+            var counterCategory =
+                PerformanceCounterCategory
+                    .GetCategories()
+                    .Where(c => CleanName(c.CategoryName) == category)
+                    .FirstOrDefault();
+
+            if (counterCategory == null)
+            {
+                return NotFound();
+            }
+            
+            var instance = GetDefaultInstance(counterCategory);
+            return Ok(counterCategory.GetCounters(instance).Select(c => c.CounterName));
+        }
+
+        [Route("perf/{category}/counters/{counter}")]
+        public bool GetCounter(string category)
+        {
+            return true;
+        }
+
+        public string CleanName(string name)
+        {
+            return Regex.Replace(name, "[:.]", "");
+        }
+
+        public string GetDefaultInstance(PerformanceCounterCategory category)
+        {
+            return category
+                .GetInstanceNames()
+                .OrderBy(n => n)
+                .FirstOrDefault();
+        }
+     }
+}
